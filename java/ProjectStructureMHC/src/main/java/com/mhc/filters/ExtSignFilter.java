ea0f79af245a7081ce7edf0e7f860e114b92bd4e
bruno <bruno.argenta@cavepot.com>
2018-04-03 15:23:43 -0300

    no message

--- java/ProjectStructureMHC/src/main/java/com/mhc/filters/ExtSignFilter.java
+++ java/ProjectStructureMHC/src/main/java/com/mhc/filters/ExtSignFilter.java
@@ -114,7 +114,9 @@ public class ExtSignFilter implements Filter {
 			httpAccessLogsDAO.saveLogs(docLogDTO);*/
 			String uuid = UUID.randomUUID().toString();
 			Cookie cookie = new Cookie(messageSource.getMessage(Constants.COOKIE_NAME, null, null), uuid);
-			//HttpSession session = httpServletRequest.getSession();
+			//cookie.setPath("/comed/rest/private/");
+			cookie.setHttpOnly(true);
+			//HttpSession session = httpServletRequest.getSession(true);
 			//session.setAttribute(uuid, "something");
 			httpServletResponse.addCookie(cookie);
 			httpServletResponse.sendRedirect(messageSource.getMessage(Constants.BIOMETRICS_URL, null, null));
