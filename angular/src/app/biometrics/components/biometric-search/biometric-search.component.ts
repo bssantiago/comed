5fba37857bcb152a9dfcbdd21b554be8a8b9969c
santiago bermudez <bssantiago@gmail.com>
2018-04-12 22:42:06 -0300

    fix

--- angular/src/app/biometrics/components/biometric-search/biometric-search.component.ts
+++ angular/src/app/biometrics/components/biometric-search/biometric-search.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IUserSearch, IKeyValues, IUserInfo } from '../../../shared/interfaces/user-info';
 import { BiometricService } from '../../services/biometric.service';
 import { map } from 'lodash';
+import { CompleterService, CompleterData } from 'ng2-completer';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/delay';
 
 @Component({
   selector: 'app-biometric-search',
@@ -10,17 +14,32 @@ import { map } from 'lodash';
 })
 export class BiometricSearchComponent implements OnInit {
 
-  public firstNames: Array<string> = [];
+  public firstNames: any;
   public lastNames: Array<string> = [];
   public user: IUserSearch;
   public drawTypes: Array<IKeyValues> = [];
   public clients: Array<IKeyValues> = [];
   public programs: Array<IKeyValues> = [];
+  protected dataService: CompleterData;
 
   public header: Array<string> = ['memberId', 'name', 'lastname', 'address'];
   public tableData: Array<any> = [];
 
-  constructor(private bservice: BiometricService) { }
+  private searchData = [
+    { color: 'red', value: '#f00' },
+    { color: 'green', value: '#0f0' },
+    { color: 'blue', value: '#00f' },
+    { color: 'cyan', value: '#0ff' },
+    { color: 'magenta', value: '#f0f' },
+    { color: 'yellow', value: '#ff0' },
+    { color: 'black', value: '#000' }
+  ];
+
+  constructor(private bservice: BiometricService, private completerService: CompleterService) {
+
+    this.dataService = completerService.local(this.getFirstNames(), 'color', 'color');
+
+  }
 
   ngOnInit() {
     this.user = {};
@@ -39,19 +58,35 @@ export class BiometricSearchComponent implements OnInit {
     }
   }
 
+  public Selected(item: any) {
+    console.log(item);
+  }
+
   public getFirstNames() {
+    return Observable.from([this.searchData]).delay(3000);
+    /*
     if (this.user.name.length > 2) {
       this.bservice.getFirstNames(this.user.name).subscribe((data: Array<string>) => {
-        this.firstNames = data;
+        const info = map(data, (item: string) => {
+          return {
+            title: item, id: item,
+          };
+        });
       });
     }
-
+*/
   }
 
   public getLastNames() {
     if (this.user.lastname.length > 2) {
       this.bservice.getLastNames(this.user.lastname).subscribe((data: Array<string>) => {
-        this.firstNames = data;
+        this.lastNames = map(data, (item: string) => {
+          return {
+            searchText: item,
+            name: item,
+            id: item
+          };
+        });
       });
     }
   }
