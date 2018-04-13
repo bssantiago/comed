6bf9d48b7f32a02c967adea0b2d82942dd1a04b2
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2018-04-13 19:24:58 -0300

    FIrst version of pagination on dynamic table

--- angular/src/app/shared/components/dynamic-table/dynamic-table.component.ts
+++ angular/src/app/shared/components/dynamic-table/dynamic-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { IKeyValues } from '../../interfaces/user-info';
 
 @Component({
@@ -6,7 +6,7 @@ import { IKeyValues } from '../../interfaces/user-info';
   templateUrl: './dynamic-table.component.html',
   styleUrls: ['./dynamic-table.component.css']
 })
-export class DynamicTableComponent implements OnInit {
+export class DynamicTableComponent implements OnInit, OnChanges {
 
   @Output() notifyChangePage: EventEmitter<number> = new EventEmitter<number>();
   @Input() public data: Array<any>;
@@ -14,13 +14,17 @@ export class DynamicTableComponent implements OnInit {
   @Input() public page = 1;
   @Input() public pageSize: number;
   @Input() public pages: number;
-
-  public currentPage;
+  private PAGINATION_COUNT = 5;
+  public pages_list = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.createPaginationList();
+  }
 
+  ngOnChanges() {
+    this.createPaginationList();
   }
 
   changePage(page) {
@@ -28,4 +32,14 @@ export class DynamicTableComponent implements OnInit {
     this.notifyChangePage.emit(page);
   }
 
+  createPaginationList() {
+    this.pages_list = [];
+    let start = 1;
+    if (this.page > Math.floor(this.PAGINATION_COUNT / 2)) {
+      start = this.page - Math.floor(this.PAGINATION_COUNT / 2);
+    }
+    for (let i = start; i < start + this.PAGINATION_COUNT && i <= this.pages; i++) {
+      this.pages_list.push(i);
+    }
+  }
 }
