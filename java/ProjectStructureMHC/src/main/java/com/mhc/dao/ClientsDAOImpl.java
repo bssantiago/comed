216f93a375069974ab607207277aaaba8b1e9bdc
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2018-04-25 16:39:20 -0300

    Add valdiation when client doesnt exist

--- java/ProjectStructureMHC/src/main/java/com/mhc/dao/ClientsDAOImpl.java
+++ java/ProjectStructureMHC/src/main/java/com/mhc/dao/ClientsDAOImpl.java
@@ -1,8 +1,11 @@
 package com.mhc.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.mhc.dto.ClientDTO;
 
@@ -13,12 +16,27 @@ public class ClientsDAOImpl extends BaseDAO<ClientsDAO> implements ClientsDAO {
     		+ "inner join comed_client_assessment cca "
     		+ "on cc.id = cca.client_id "
     		+ "where  cca.status = true";
-
-	@Override
+    
+    private static final String SELECT_CLIENT_BY_ID = "SELECT * FROM comed_client WHERE id = :id";
+    
+    @Override
 	public List<ClientDTO> getClients() {
 		List<ClientDTO> clients  = jdbcTemplate.query(SELECT_CLIENTS,
 				new BeanPropertyRowMapper<ClientDTO>(ClientDTO.class));
 		return clients;
+	}
+	
+	public ClientDTO getClient(int id) {
+		ClientDTO client = null;
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		SqlRowSet srs = namedParameterJdbcTemplate.queryForRowSet(SELECT_CLIENT_BY_ID, params);
+		if (srs.next()) {
+			client = new ClientDTO();
+			client.setId(srs.getLong("quantity"));
+			//TODO: set all other parameters
+		}
+		return client;
 	}
 
 	@Override
