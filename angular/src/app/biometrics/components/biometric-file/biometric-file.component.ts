07f7d2dc15a69323f2499176850f31464de544de
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2018-04-30 10:42:35 -0300

    no message

--- angular/src/app/biometrics/components/biometric-file/biometric-file.component.ts
+++ angular/src/app/biometrics/components/biometric-file/biometric-file.component.ts
@@ -104,6 +104,7 @@ export class BiometricFileComponent implements OnInit {
 
   public upload(model: IFile, isValid: boolean) {
     if (isValid) {
+      this.toast.success('You are uploading a new eligibility file and will remove all previous eligibility files for this client', '');
       const request = this.clientAssessmentMapper(model)[0];
       this.bservice.upload(request).subscribe(res => {
         this.refreshGrid();
