3c31e02b556fa1610e168c8154e17cf1e3912629
bruno <bruno.argenta@cavepot.com>
2018-05-15 11:06:22 -0300

    no message

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -4,8 +4,8 @@ import { ForbiddenComponent } from './components/forbidden/forbidden.component';
 import { AuthenticateComponent } from './components/authenticate/authenticate.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/not-found', pathMatch: 'full' },
+  { path: 'not-found', component: HomeComponent },
   { path: 'forbidden', component: ForbiddenComponent },
   { path: 'forbidden/:client', component: ForbiddenComponent },
   { path: 'authenticate/:token/:nonce/:sk/:requestBy/:signature/:client/:patient', component: AuthenticateComponent},
